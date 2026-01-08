95d4e3fcb90c77514822ecc9fc3d18bd2883237a
done778 <57587831+done778@users.noreply.github.com>
2026-01-09 01:51:16 +0900

    [feat] RPC GameResult Method
    - 승리, 패배 버튼으로 분리
    - 누군가 하나를 상호작용하면 RPC로 모두에게 결과 전송

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public enum SceneState
 {
@@ -19,6 +19,8 @@ public class GameManager : Singleton<GameManager>
 
     public PhotonPunManager Pun { get; private set; }
 
+    private GameResultController _controller;
+
     protected override void OnSingletonAwake()
     {
         Pun = GetComponent<PhotonPunManager>();
@@ -34,5 +36,45 @@ public class GameManager : Singleton<GameManager>
     public void SetSceneState(SceneState state)
     {
         _flowState = state;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    // 이 아래로 게임 결과 관련 메서드들
+    // TODO : 시민과 해적의 승리로 나뉘므로 RPC로 뿌릴 때 자신의 역할에 따라 다른 결과가 나와야함.
+    // 위 부분을 아직 고려 안한 상태
+    public void RegistResultPanel(GameResultController controller)
+    {
+        _controller = controller;
+    }
+
+    public void GameOverAndResult(bool isCitizenVictory)
+    {
+        if (isCitizenVictory)
+            CitizenVictory();
+        else
+            PiratesVictory();
+    }
+
+    // 시민의 승리
+    public void CitizenVictory()
+    {
+        _controller.Victory();
+        PauseGame();
+    }
+
+    // 해적의 승리
+    public void PiratesVictory()
+    {
+        _controller.Defeat();
+        PauseGame();
     }
 }
