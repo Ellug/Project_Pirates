dabaf2353e8e52513a0396300154ece9b257f15e
HoChan8253 <nahcohochan@gmail.com>
2026-01-14 16:30:05 +0900

    [WIP] Networking for chat system
    채팅 네트워크화 구현 시작

---
+++ Assets/_Scripts/UI/ChatNetwork.cs
@@ -0,0 +1,27 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+
+// RPC로 방 전체 전달
+public class ChatNetwork : MonoBehaviourPun
+{
+    [SerializeField] private ChatLogView _logView;
+    //[SerializeField] private int _maxChars = 80;
+
+    // ChatInput 에서 호출
+    public void SendChat(string rawText)
+    {
+        if (!PhotonNetwork.InRoom) return;
+
+        string text = (rawText ?? "").Trim();
+        if (string.IsNullOrEmpty(text)) return;
+
+        string nickName = PhotonNetwork.LocalPlayer?.NickName;
+        if (string.IsNullOrEmpty(nickName))
+            nickName = PhotonNetwork.LocalPlayer?.UserId ?? "Unknown";
+
+        int actorNumber = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        //photonView.RPC()
+    }
+}
