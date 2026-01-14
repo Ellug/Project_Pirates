37a8b4753fe4a4fe0d0fa9f2e05f481c9cb050db
done778 <57587831+done778@users.noreply.github.com>
2026-01-14 12:48:53 +0900

    [feat] Add Player State Pattern Class
    플레이어 상태 패턴 구현을 위한 클래스들 우선 추가

--- Assets/_Scripts/Player/PlayerContoller.cs
+++ Assets/_Scripts/Player/PlayerContoller.cs
@@ -10,15 +10,40 @@ public class PlayerContoller : MonoBehaviourPunCallbacks
     [SerializeField] private float _mouseSensitivity;
 
     private Vector2 _inputMove;
+    private Vector2 _mouseDelta;
     private float _xRotation;
     private Camera _camera;
     private PhotonView _view;
     private PlayerInteraction _playerInteraction;
+    private PlayerStateMachine _stateMachine;
     private Animator _animator;
+
+    // Player State
+    private IdleState _idle;
+    private MoveState _move;
+    private JumpState _jump;
+    private CrouchState _crouch;
 
     private void Awake()
     {
         _view = GetComponent<PhotonView>();
+
+        if (!_view.IsMine)
+            return;
+
+        _stateMachine = new PlayerStateMachine(_idle);
+        _idle = new IdleState();
+        _move = new MoveState();
+        _jump = new JumpState();
+        _crouch = new CrouchState();
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _camera = Camera.main;
+        _camera.transform.SetParent(transform, false);
+        _camera.transform.localPosition = new Vector3(0f, 1.77f, 0f);
+
+        _inputMove = Vector2.zero;
     }
 
     void Start()
@@ -28,14 +53,6 @@ public class PlayerContoller : MonoBehaviourPunCallbacks
 
         _animator = GetComponent<Animator>();
         _playerInteraction = GetComponent<PlayerInteraction>();
-        
-        Cursor.lockState = CursorLockMode.Locked;
-        
-        _camera = Camera.main;
-        _camera.transform.SetParent(transform, false);
-        _camera.transform.localPosition = new Vector3(0f, 1.77f, 0f);
-        
-        _inputMove = Vector2.zero;
 
         InputSystem.actions["Move"].performed += OnMove;
         InputSystem.actions["Move"].canceled += OnMove;
@@ -60,10 +77,28 @@ public class PlayerContoller : MonoBehaviourPunCallbacks
         PlayerMove();
     }
 
+    private void LateUpdate()
+    {
+        PlayerLook();
+    }
+
     private void PlayerMove()
     {
         Vector3 dir = new Vector3(_inputMove.x, 0f, _inputMove.y).normalized;
         transform.Translate(Time.fixedDeltaTime * _moveSpeed * dir);
+    }
+
+    private void PlayerLook()
+    {
+        float mouseX = _mouseDelta.x * _mouseSensitivity * Time.deltaTime;
+        float mouseY = _mouseDelta.y * _mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f); // 위아래 90도 제한
+
+        // 카메라와 플레이어 몸체에 회전 적용
+        _camera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f); // 카메라만 위아래로
+        transform.Rotate(Vector3.up * mouseX); // 플레이어 몸체 전체가 좌우로 회전
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -87,16 +122,6 @@ public class PlayerContoller : MonoBehaviourPunCallbacks
 
     private void OnLook(InputAction.CallbackContext ctx)
     {
-        Vector2 mouseDelta = ctx.ReadValue<Vector2>();
-
-        float mouseX = mouseDelta.x * _mouseSensitivity * Time.deltaTime;
-        float mouseY = mouseDelta.y * _mouseSensitivity * Time.deltaTime;
-
-        _xRotation -= mouseY;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f); // 위아래 90도 제한
-
-        // 카메라와 플레이어 몸체에 회전 적용
-        _camera.transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f); // 카메라만 위아래로
-        transform.Rotate(Vector3.up * mouseX); // 플레이어 몸체 전체가 좌우로 회전
+        _mouseDelta = ctx.ReadValue<Vector2>();
     }
 }
---
+++ Assets/_Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public interface IPlayerState
+{
+    public void Enter();
+    public void HandleInput();
+    public void FrameUpdate();
+    public void PhysicsUpdate();
+    public void Exit();
+}
+
+public class PlayerStateMachine
+{
+    public IPlayerState CurrentState { get; private set; }
+
+    public PlayerStateMachine(IPlayerState initState)
+    {
+        CurrentState = initState;
+        CurrentState.Enter();
+    }
+
+    public void ChangeState(IPlayerState newState)
+    {
+        CurrentState.Exit();
+        CurrentState = newState;
+        CurrentState.Enter();
+    }
+}
+
+public class IdleState : IPlayerState
+{
+    public void Enter() { }
+    public void HandleInput() { }
+    public void FrameUpdate() { }
+    public void PhysicsUpdate() { }
+    public void Exit() { }
+}
+
+public class MoveState : IPlayerState
+{
+    public void Enter() { }
+    public void HandleInput() { }
+    public void FrameUpdate() { }
+    public void PhysicsUpdate() { }
+    public void Exit() { }
+}
+
+public class JumpState : IPlayerState
+{
+    public void Enter() { }
+    public void HandleInput() { }
+    public void FrameUpdate() { }
+    public void PhysicsUpdate() { }
+    public void Exit() { }
+}
+
+public class CrouchState : IPlayerState
+{
+    public void Enter() { }
+    public void HandleInput() { }
+    public void FrameUpdate() { }
+    public void PhysicsUpdate() { }
+    public void Exit() { }
+}
