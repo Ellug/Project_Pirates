d9dad6a549e34d0d7475ff83d9facdb7191aaf18
KangMin9704 <wnrkdals123@gmail.com>
2026-01-15 11:57:28 +0900

    [Feat] AddScene VoiceTest
    보이스 테스트 커밋

---
+++ Assets/_Scripts/Voice/VoiceManager.cs
@@ -0,0 +1,25 @@
+﻿using Photon;
+using Photon.Pun;
+using Photon.Voice;
+using Photon.Voice.Unity;
+using UnityEngine;
+
+public class VoiceManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Recorder recorder;
+    [SerializeField] private Speaker speaker;
+
+    private void Start()
+    {
+        if (recorder == null || speaker == null)
+        {
+            Debug.LogError("Recorder 또는 Speaker가 없습니다.");
+            return;
+        }
+
+        recorder.DebugEchoMode = true;
+        recorder.TransmitEnabled = true;
+
+        Debug.Log("Photon Voice Debug Echo 활성화됨");
+    }
+}
