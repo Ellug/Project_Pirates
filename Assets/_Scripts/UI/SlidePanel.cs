769d68bac229122e271fd84fd0475fa4dcf031a1
HoChan8253 <nahcohochan@gmail.com>
2026-01-22 00:12:40 +0900

    [WIP] work in progress on dotween slide panel
    DOTween 슬라이드 패널 작업 작성 중
    Merge를 통해 최신화를 하기 위한 커밋

---
+++ Assets/_Scripts/UI/SlidePanel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using Unity.VisualScripting;
+
+public class SlidePanel : MonoBehaviour
+{
+    [SerializeField] private RectTransform _panel;
+    [SerializeField] private float _duration = 0.25f;
+    [SerializeField] private float _hiddenX = 600f;
+    [SerializeField] private float _shownX = 0f;
+
+    private bool _isOpen;
+    private Tween _tween;
+
+    private void Reset()
+    {
+        _panel = GetComponent<RectTransform>();
+    }
+
+    private void Awake()
+    {
+        if (_panel == null) _panel = GetComponent<RectTransform>();
+
+        _panel.anchoredPosition = new Vector2(_hiddenX, _panel.anchoredPosition.y);
+        _isOpen = false;
+    }
+
+    public void Toggle()
+    {
+        SetOpen(!_isOpen);
+    }
+
+    public void SetOpen(bool open)
+    {
+        _isOpen = open;
+
+        _tween?.Kill();
+
+        float targetX = _isOpen ? _shownX : _hiddenX;
+        _tween = _panel.DOAnchorPosX(targetX, _duration)
+                       .SetEase(Ease.OutCubic)
+                       .SetUpdate(true);
+    }
+}
