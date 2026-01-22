5bcd52df52ca388c501dc71ddf3bcf0419a319d2
Ellug <auron975@nate.com>
2026-01-22 12:55:54 +0900

    [Fix] InGame : Result Panel Active fix

--- Assets/_Scripts/UI/GameResultController.cs
+++ Assets/_Scripts/UI/GameResultController.cs
@@ -4,20 +4,22 @@ using TMPro;
 public class GameResultController : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _resultText;
+    [SerializeField] GameObject _resultPanel;
+
     void Start()
     {
         GameManager.Instance.RegistResultPanel(this);
-        _resultText.gameObject.SetActive(false);
+        _resultPanel.SetActive(false);
     }
 
     public void Victory()
     {
         _resultText.text = "승리";
-        _resultText.gameObject.SetActive(true);
+        _resultPanel.SetActive(true);
     }
     public void Defeat()
     {
         _resultText.text = "패배";
-        _resultText.gameObject.SetActive(true);
+        _resultPanel.SetActive(true);
     }
 }
