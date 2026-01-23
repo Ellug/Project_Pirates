c2a20cf07b49a70b5bec7ee0b2bf9894e680af36
HoChan8253 <nahcohochan@gmail.com>
2026-01-23 17:38:34 +0900

    [Fix] Sabotage time using coroutine
    사보타지 타이머 감소 로직

---
+++ Assets/_Scripts/InGame/Sabotage/SabotageButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SabotageButton : MonoBehaviour
+{
+    [SerializeField] private SabotageManager _sabotageManager;
+    [SerializeField] private SabotageId _sabotageId;
+
+    public void Trigger()
+    {
+        Debug.Log($"[UI] Sabotage Button Clicked : {_sabotageId}");
+
+        _sabotageManager.TriggerSabotage(_sabotageId);
+    }
+}
--- Assets/_Scripts/InGame/Sabotage/SabotageManager.cs
+++ Assets/_Scripts/InGame/Sabotage/SabotageManager.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using TMPro;
+using System;
 using UnityEngine;
+using System.Collections;
+
+// 트리거 되는 순간 코루틴으로 waitforsec 로 시간 카운트 
+// 중간에 시민이 활성화하면 yeild break; 
+// 타이머 초기화 
+// 코루틴을 빠져나오면 false = 마피아 승리
 
 public enum SabotageId
 {
@@ -19,20 +26,19 @@ public class SabotageManager : MonoBehaviour
     [Header("Config")]
     [SerializeField] private float _defaultDuration = 60f;
 
+    [Header("Countdown UI")]
+    [SerializeField] private GameObject _countdownPanel;
+    [SerializeField] private TMP_Text _countdownText;
+
     private bool _isActive;
     private SabotageId _activeId = SabotageId.None;
     private float _remainingTime;
 
-    private void Update()
+    private Coroutine _countdownCor;
+
+    private void Awake()
     {
-        if (!_isActive) return;
-
-        _remainingTime -= Time.deltaTime;
-
-        if(_remainingTime <= 0f)
-        {
-            FailSabotage();
-        }
+        SetCountdownUI(false);
     }
 
     public bool TriggerSabotage(SabotageId id, float duration = -1f)
@@ -45,14 +51,72 @@ public class SabotageManager : MonoBehaviour
         _remainingTime = duration > 0f ? duration : _defaultDuration;
 
         OnSabotageStart(id);
+        StartCountdownCoroutine();
         return true;
     }
+
+    #region Coroutine
+    private void StartCountdownCoroutine()
+    {
+        StopCountdownCoroutine();
+        _countdownCor = StartCoroutine(Co_countdown());
+    }
+
+    private void StopCountdownCoroutine()
+    {
+        if (_countdownCor != null)
+        {
+            StopCoroutine(_countdownCor);
+            _countdownCor = null;
+        }
+    }
+
+    private IEnumerator Co_countdown()
+    {
+        SetCountdownUI(true);
+        UpdateCountdownUI(_remainingTime);
+
+        // waitforsec으로 카운트다운
+        while (_isActive && _remainingTime > 0f)
+        {
+            _remainingTime -= Time.deltaTime;
+            UpdateCountdownUI(_remainingTime);
+            yield return null;
+        }
+
+        // 코루틴을 빠져나오면 실패
+        if(_isActive)
+        {
+            FailSabotage();
+        }
+    }
+
+    private void SetCountdownUI(bool on)
+    {
+        if (_countdownPanel != null)
+            _countdownPanel.SetActive(on);
+    }
+
+    private void UpdateCountdownUI(float time)
+    {
+        if (_countdownText == null) return;
+
+        if (time < 0f) time = 0f;
+
+        int total = Mathf.CeilToInt(time);
+        int m = total / 60;
+        int s = total & 60;
+        _countdownText.text = $"{m:00} : {s:00}";
+    }
+    #endregion
 
     public bool ResolveSabotage(SabotageId id)
     {
         if (!_isActive) return false;
         if (_activeId != id) return false;
 
+        // 시민이 해결하면 코루틴 중단
+        StopCountdownCoroutine();
         SuccessSabotage();
         return true;
     }
