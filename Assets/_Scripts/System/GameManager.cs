e97ea32d9a464c02194aa0888cd02411d0f98b80
KangMin9704 <wnrkdals123@gmail.com>
2026-01-28 18:14:20 +0900

    [Feat] 페이드 기능 구현
     - 인게임씬 진입, 퇴장 시 페이드 처리

--- Assets/_Scripts/System/GameManager.cs
+++ Assets/_Scripts/System/GameManager.cs
@@ -17,7 +17,9 @@ public class GameManager : Singleton<GameManager>
 
     public PhotonPunManager Pun { get; private set; }
 
-    private GameResultController _controller;
+    //private GameResultController _controller;
+
+    private FadeController _fadeController;
 
     protected override void OnSingletonAwake()
     {
@@ -34,33 +36,43 @@ public class GameManager : Singleton<GameManager>
 
 
     // 네트워크 게임이기에 Time.timeScale로 퍼즈하는 것 보단 게임 종료 연출을 따로 만드는 게 좋을 거 같음
-    public void PauseGame()
-    {
-        Time.timeScale = 0f;
-    }
+    //public void PauseGame()
+    //{
+    //    Time.timeScale = 0f;
+    //}
 
-    public void ResumeGame()
-    {
-        Time.timeScale = 1f;
-    }
+    //public void ResumeGame()
+    //{
+    //    Time.timeScale = 1f;
+    //}
 
     // 이 아래로 게임 결과 관련 메서드들
     // TODO : 시민과 해적의 승리로 나뉘므로 RPC로 뿌릴 때 자신의 역할에 따라 다른 결과가 나와야함.
     // 위 부분을 아직 고려 안한 상태
-    public void RegistResultPanel(GameResultController controller)
+    //public void RegistResultPanel(GameResultController controller)
+    //{
+    //    _controller = controller;
+    //}
+
+    //public void Victory()
+    //{
+    //    _controller.Victory();
+    //    GameOver();
+    //}
+
+    //public void Defeat()
+    //{
+    //    _controller.Defeat();
+    //    GameOver();
+    //}
+    public void SetFadingController(FadeController fadeController)
     {
-        _controller = controller;
+        _fadeController = fadeController;
     }
 
-    public void Victory()
+    public void EndGame(bool isWin)
     {
-        _controller.Victory();
-        GameOver();
-    }
-
-    public void Defeat()
-    {
-        _controller.Defeat();
+        _fadeController.EndInGameFade(isWin);
         GameOver();
     }
 
@@ -69,9 +81,9 @@ public class GameManager : Singleton<GameManager>
         if(PlayerManager.Instance != null)
             PlayerManager.Instance.GameOver();
 
-        if (InputManager.Instance != null)
-            InputManager.Instance.SetUIMode(true);
+        //if (InputManager.Instance != null)
+        //    InputManager.Instance.SetUIMode(true);
 
-        PauseGame();
+        //PauseGame();
     }
 }
--- Assets/_Scripts/UI/GameResultController.cs
+++ Assets/_Scripts/UI/GameResultController.cs
@@ -1,25 +1,25 @@
-﻿using UnityEngine;
-using TMPro;
+﻿//using UnityEngine;
+//using TMPro;
 
-public class GameResultController : MonoBehaviour
-{
-    [SerializeField] TextMeshProUGUI _resultText;
-    [SerializeField] GameObject _resultPanel;
+//public class GameResultController : MonoBehaviour
+//{
+//    [SerializeField] TextMeshProUGUI _resultText;
+//    [SerializeField] GameObject _resultPanel;
 
-    void Start()
-    {
-        GameManager.Instance.RegistResultPanel(this);
-        _resultPanel.SetActive(false);
-    }
+//    void Start()
+//    {
+//        GameManager.Instance.RegistResultPanel(this);
+//        _resultPanel.SetActive(false);
+//    }
 
-    public void Victory()
-    {
-        _resultText.text = "승리";
-        _resultPanel.SetActive(true);
-    }
-    public void Defeat()
-    {
-        _resultText.text = "패배";
-        _resultPanel.SetActive(true);
-    }
-}
+//    public void Victory()
+//    {
+//        _resultText.text = "승리";
+//        _resultPanel.SetActive(true);
+//    }
+//    public void Defeat()
+//    {
+//        _resultText.text = "패배";
+//        _resultPanel.SetActive(true);
+//    }
+//}
