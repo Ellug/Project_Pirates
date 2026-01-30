e5c0cdb89d47137b115ca63df19257c197a833d2
Ellug <auron975@nate.com>
2026-01-30 12:17:20 +0900

    [Fix] OptionPanel, Interaction bug fix

--- Assets/_Scripts/InGame/Mission/AjeMission.cs
+++ Assets/_Scripts/InGame/Mission/AjeMission.cs
@@ -5,6 +5,8 @@ public class AjeMission : MissionBase
 {
     [SerializeField] private TMP_Text _curProblem;
     [SerializeField] private TMP_InputField _answerInput;
+    [SerializeField] private TMP_Text _curAnswer;
+    [SerializeField] private TMP_Text _resultText;
 
     private readonly (string problem, string answer)[] _quizList =
     {
@@ -26,14 +28,13 @@ public class AjeMission : MissionBase
         ("소가 군대 가면?", "육군훈련소"),
         ("소가 발이 두개면?", "이발소"),
         ("소가 계단을 오르면?", "소오름"),
-        ("소가 커피를 마시면면?", "에스프레소"),
+        ("소가 커피를 마시면?", "에스프레소"),
         ("소가 번개를 맞으면?", "우사인볼트"),
         ("소가 노래를 부르면?", "소송"),
         ("소가 단체로 노래를 부르면?", "단체소송"),
         ("[로스트아크] 할족이 승차거부 당하면?", "타지마할"),
         ("[로스트아크] 할족이 승차요구 당하면?", "할타"),
         ("[로스트아크] 할족 자식들이 싸우는 이유는?", "상속분할"),
-        ("[로스트아크] 할족이 주식투자 할때 자주 쓰는 기법은?", "분할매수"),
         ("[로스트아크] 할족이 주식투자 할때 자주 쓰는 기법은?", "분할매수"),
         ("[로스트아크] 할족의 아버지는?", "할아버지"),
         ("[로스트아크] 할족이 호크아이 클래스를 선택하면?", "할매"),
@@ -44,26 +45,35 @@ public class AjeMission : MissionBase
         ("왕이 넘어지면?", "킹콩"),
         ("자가용의 반대말은?", "커용"),
         ("4가 도망가면?", "포도주"),
-        ("4가 도망가면?", "포도주"),
         ("한남에 자가 있는 가수는?", "김종국"),
         ("치아가 잘 보이는 사람은?", "이보영"),
         ("씨엘의 엉덩이를 영어로?", "class"),
-        ("씨엘의 엉덩이를 영어로?", "class"),
     };
-
-    private string _currentAnswer;
 
     public override void Init()
     {
+        _resultText.text = "";
+
         int index = Random.Range(0, _quizList.Length);
         _curProblem.text = _quizList[index].problem;
-        _currentAnswer = _quizList[index].answer;
+        _curAnswer.text = _quizList[index].answer;
         _answerInput.text = "";
+        
+        _curAnswer.gameObject.SetActive(false);
     }
 
     public void OnClickSubmit()
     {
-        if (_answerInput.text == _currentAnswer)
-            CompleteMission();
+        _curAnswer.gameObject.SetActive(true);
+
+        if (_answerInput.text != _curAnswer.text)
+        {
+            _resultText.text = $"오답입니다ㅋ 정답은 {_curAnswer.text} 깔깔깔";
+            MissionContainer.Instance.CloseMissionPanel();
+            return;
+        }
+
+        _resultText.text = "이걸 맞추네...";
+        CompleteMission();
     }
 }
