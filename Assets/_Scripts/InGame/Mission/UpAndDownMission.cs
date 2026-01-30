8b81d4d0d8f49fdd87b935b6ca82356da582776f
HoChan8253 <nahcohochan@gmail.com>
2026-01-30 10:05:47 +0900

    [WIP] Merge branch into develop
    머지하기 위한 커밋 작성

---
+++ Assets/_Scripts/InGame/Mission/UpAndDownMission.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpAndDownMission : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TMP_InputField _input;
+    [SerializeField] private TMP_Text _resultText;
+    [SerializeField] private TMP_Text _chanceText;
+    [SerializeField] private Button _submitButton;
+
+    private int _answer;
+    private int _remainChance = 7;
+
+    //public override void Init()
+    //{
+    //    _answer = Random.Range(1, 101);
+    //    _remainChance = 7;
+    //
+    //    _resultText.text = "1부터 100 사이 숫자를 입력해주세요.";
+    //
+    //}
+}
